18c8cfcf92e27aa8985278de68d1565b03367d32
Suyog Gadhave <suyog.gadhave@zycus.com>
2018-07-26 15:16:13 +0530

    Simple thread

--- ThreadDemo/src/com/zycus/Main2.java
+++ ThreadDemo/src/com/zycus/Main2.java
@@ -3,28 +3,41 @@ package com.zycus;
 public class Main2 {
 
 	public static void main(String[] args) {
-		Repeater a = new Repeater("India");
-		Repeater b = new Repeater("Australia");
+		// Repeater a = new Repeater("India");
+		// Repeater b = new Repeater("Australia");
+		//
+		// // System.out.println(Thread.currentThread().getName());
+		//
+		// Thread t1 = new Thread(a);
+		// // t1.setDaemon(true);
+		// t1.start();
+		// new Thread(b).start();
+		//
+		// try {
+		// for (int i = 0; i < 10; i++) {
+		// System.out.println("MAIN THREAD");
+		// Thread.sleep(100);
+		// }
+		// } catch (InterruptedException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		//
+		// System.out.println(Thread.currentThread().getName() + " ended!");
 
-		// System.out.println(Thread.currentThread().getName());
-
-		Thread t1 = new Thread(a);
-		// t1.setDaemon(true);
-		t1.start();
-		new Thread(b).start();
-
-		try {
-			for (int i = 0; i < 10; i++) {
-				System.out.println("MAIN THREAD");
-				Thread.sleep(100);
+		new Thread(() -> {
+			System.out.println("Thread :" + Thread.currentThread().getName());
+			for (int i = 0; i < 100; i++) {
+				System.out.println("Inside0");
 			}
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		}).start();
 
-		System.out.println(Thread.currentThread().getName() + " ended!");
-
+		new Thread(() -> {
+			System.out.println("Thread :" + Thread.currentThread().getName());
+			for (int i = 0; i < 100; i++) {
+				System.out.println("Inside1");
+			}
+		}).start();
 	}
 
 }
